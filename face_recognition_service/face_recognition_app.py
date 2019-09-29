447cf0a111d7fb00bea917016ad9fe375e6a0b73
zhanghailong22 <zhanghailong22@huawei.com>
2019-09-29 19:24:35 +0800

    commit

--- face_recognition_service/face_recognition_app.py
+++ face_recognition_service/face_recognition_app.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+# This is an app application for face recognition microservices.
+# Its functions are: face upload, face recognition, Video face recognition, etc.
+# The restfull api is http://127.0.0.1:9999. Database is redis, used to store
+# the feature vector of the face.
+# Author e-mail:zhanghailong22@huawei.com
+#
 import os
 
 import video_camera
@@ -8,11 +14,10 @@ import redis
 import face_recognition
 import numpy as np
 
-
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 pool = redis.ConnectionPool(host='127.0.0.1', port=6379)
-#pool = redis.ConnectionPool(host='redis', port=6379)
+# pool = redis.ConnectionPool(host='redis', port=6379)
 @app.route('/upload', methods=['POST'])
 def upload():
     if 'file' not in request.files:
@@ -29,23 +34,22 @@ def upload():
         image = face_recognition.load_image_file(file)
         face_locations = face_recognition.face_locations(image)
         if len(face_locations) != 1:
-            return jsonify({'code': 500, 'msg': '人脸数量有误'})
+            return jsonify({'code': 500, 'error': '人脸数量有误'})
         face_encodings = face_recognition.face_encodings(image, face_locations)
         # 连数据库
         r = redis.Redis(connection_pool=pool)
         # 录入人名-对应特征向量
         r.set(name, face_encodings[0].tobytes())
-    return jsonify({'code': 0, 'msg': '录入成功'})
-#人脸搜索
+    return jsonify({'code': 0, 'result': '录入成功'})
+
+# 人脸搜索
 @app.route('/search_images', methods=['POST'])
 def search_practice():
     if 'file' not in request.files:
-        return jsonify({'code': 500, 'msg': '没有文件'})
+        return jsonify({'code': 500, 'result': '没有文件'})
     file = request.files['file']
     image = face_recognition.load_image_file(file)
     face_locations = face_recognition.face_locations(image)
-    if len(face_locations) != 1:
-        return jsonify({'code': 500, 'msg': '人脸数量有误'})
     face_encodings = face_recognition.face_encodings(image, face_locations)
     # 连数据库
     r = redis.Redis(connection_pool=pool)
@@ -53,16 +57,20 @@ def search_practice():
     names = r.keys()
     faces = r.mget(names)
     # 组成矩阵，计算相似度（欧式距离）
-    matches = face_recognition.compare_faces([np.frombuffer(x) for x in faces], face_encodings[0])
-    for name, match in zip(names, matches):
-        if match:
-            return jsonify({'result': 'success', 'names': str(name, 'utf-8')})
-        else:
-            return jsonify(({'result': 'failed', 'name': 'Did not find the name of the person'}))
+    find_names = []
+    number = len(face_encodings)
+    for face_encoding in face_encodings:
+        matches = face_recognition.compare_faces([np.frombuffer(x) for x in faces], face_encoding)
+        for name, match in zip(names, matches):
+            if match:
+                find_names.append(name)
+                break
 
- #   return jsonify({'code': 0, 'names': [str(name, 'utf-8') for name, match in zip(names, matches) if match]})
+    return jsonify({'图片中人脸的个数': number, '查询到的人脸个数': len(find_names), \
+                    "find_names": [str(name, 'utf-8') for name in find_names]})
+#   return jsonify({'code': 0, 'names': [str(name, 'utf-8') for name, match in zip(names, matches) if match]})
 
-
+# 视频监控
 @app.route('/search_video', methods=['POST'])
 def search_video():
     # 连数据库
@@ -72,23 +80,27 @@ def search_video():
     faces = r.mget(names)
     # 组成矩阵，计算相似度（欧式距离）
     face_names = video_camera.video_camera(names, faces)
+
     return jsonify({'names': face_names})
-#更新redis
+
+# 更新redis，将人脸图片的特征向量导入redis数据库
 @app.route('/update_redis', methods=['POST'])
 def update_redis():
     directory_name="images"
+    # 连数据库
+    r = redis.Redis(connection_pool=pool)
     for filename in os.listdir(directory_name):
         file = open(directory_name + "/" + filename, 'rb')
         name = filename[0:-4]
         image = face_recognition.load_image_file(file)
         face_locations = face_recognition.face_locations(image)
         face_encodings = face_recognition.face_encodings(image, face_locations)
-        # 连数据库
-        r = redis.Redis(connection_pool=pool)
+
         # 录入人名-对应特征向量
         r.set(name, face_encodings[0].tobytes())
-    return jsonify({'code': 0, 'msg': '更新成功'})
+    names = r.keys()
+
+    return jsonify({'redis中人脸数目': len(names), 'result': '更新成功'})
 
 if __name__ == '__main__':
-
     app.run(host="0.0.0.0", debug=True, port=9999)
--- face_recognition_service/test.py
+++ face_recognition_service/test.py
@@ -1,6 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# test face_recognition_app.py
+
 import requests
 from flask import Flask, Response, request, jsonify
 
+# 录入
 url = "http://127.0.0.1:9999/upload"
 
 filepath = 'D:\code_input_gitlab\osdt-mep-app\examples/biden.jpg'
@@ -14,9 +19,10 @@ files = {'file': (filename, file, 'image/jpg')}
 r = requests.post(url, files=files)
 result = r.text
 
+# 查询
 url = "http://127.0.0.1:9999/search_images"
 
-filepath = 'D:\code_input_gitlab\osdt-mep-app\examples/alex-lacamoire.png'
+filepath = 'D:\code_input_gitlab\osdt-mep-app\examples/two_people.jpg'
 split_path = filepath.split('/')
 filename = split_path[-1]
 print(filename)
@@ -28,12 +34,14 @@ r = requests.post(url, files=files)
 result = r.text
 print(result)
 
+# 视频
 url = "http://127.0.0.1:9999/search_video"
 
 r = requests.post(url)
 result = r.text
 print(result)
 
+# 更新redis
 url = "http://127.0.0.1:9999/update_redis"
 
 r = requests.post(url)
